9d3e2d4168fd5d7e6a576de23b00ae73276eb229
BenoitRoub <bn.roub@gmail.com>
2019-10-03 10:13:53 +0200

    new

--- server.js
+++ server.js
@@ -43,6 +43,8 @@ app.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
+console.log(port);
+
 app.listen(port, () => {
 	console.log(`Server is running on port : ${port}`);
 });
